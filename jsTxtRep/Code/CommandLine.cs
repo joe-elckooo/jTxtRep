d5191c08d062ccb8cdd43439911e5731931055e8
Joe Elckooo <40741925+joe-elckooo@users.noreply.github.com>
2020-12-30 02:15:41 +0300

    Add files via upload

---
+++ jsTxtRep/Code/CommandLine.cs
@@ -0,0 +1,210 @@
+﻿/*
+Copyright 2020 Joe-Elckooo
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+//  (2020.12.30)
+
+using System;
+using System.Collections.Generic;
+
+
+namespace jnsCommandLine
+{
+
+
+  ///++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  /// <summary>
+  /// 
+  /// </summary>
+  public readonly struct Command
+  {
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="key"></param>
+    /// <param name="value"></param>
+    public Command(string key, string value)
+    {
+      Key = key;
+      Value = value;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public readonly string Key;
+    /// <summary>
+    /// 
+    /// </summary>
+    public readonly string Value;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    public override string ToString()
+    {
+      return string.Format("{0} [{1}]", Key ?? "$", Value);
+    }
+
+  }
+
+  ///++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  /// <summary>
+  /// 
+  /// </summary>
+  public class Commands : List<Command>
+  {
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="key"></param>
+    /// <param name="value"></param>
+    public void Add(string key, string value)
+    {
+      Add(new Command(key, value));
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="key"></param>
+    /// <param name="value"></param>
+    public void Insert(int index, string key, string value)
+    {
+      Insert(index, new Command(key, value));
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public void Add()
+    {
+      Add(new Command());
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="src"></param>
+    public void Append(Commands src)
+    {
+      this.AddRange(src);
+    }
+
+  }
+
+  ///++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  /// <summary>
+  /// 
+  /// </summary>
+  public class CommandLine
+  {
+
+    ///****************************************************************************
+    /// <summary>
+    /// Parsing the command line
+    /// Format: -p -d:'Value' -s:Value, 
+    ///   where: p, d, s -- command, Value -- value
+    /// -p -d:'value' | -d:"value"
+    /// </summary>
+    /// <param name="cmd"></param>
+    /// <returns></returns>
+    public static Commands ParseCommandLine(string[] cmd)
+    {
+      Commands result = new Commands();
+
+      int i = 0, j;
+
+      string key;
+      string val;
+
+      for (; i < cmd.Length; i++)
+      {
+        key = null;
+        val = null;
+        j = 0;
+
+        if (cmd[i][0] == '-')
+        {
+          if (__get_key()) __get_value();
+        }
+        else __get_value();
+
+        if ((key != null) || (val != null)) result.Add(key, val);
+      }
+
+      return result;
+
+
+      //----->>>
+      bool __get_key()
+      {
+        string s = cmd[i];
+        char c = '\x00';
+
+        // find end of key string
+        j = 1;
+        for (; j < s.Length; j++)
+        {
+          c = s[j];
+          if ((c == ':') || (c <= ' ')) break;
+        }
+
+        key = s.Substring(1, j - 1);
+        if (key.Length == 0) key = null;
+
+        if (c == ':') ++j;
+        else if (j == s.Length)
+        {
+          int k = i + 1;
+          if ((k < cmd.Length) && (cmd[k][0] == ':'))
+          {
+            i = k; j = 1; c = ':';
+          }
+        }
+        else
+        {
+          int k = j;
+          for (; k < s.Length; k++)
+          {
+            if (cmd[i][k] == ':')
+            {
+              j = k + 1;
+              c = ':';
+              break;
+            }
+          }
+        }
+
+        return (c == ':') && (key != null);
+      }
+
+      //----->>>
+      bool __get_value()
+      {
+        if (j >= cmd[i].Length) { j = 0; ++i; }
+
+        bool r = i < cmd.Length;
+        if (r) val = cmd[i].Substring(j);
+
+        return r;
+      }
+
+    }
+
+
+  }
+}
---
+++ jsTxtRep/Code/Program.cs
@@ -0,0 +1,250 @@
+﻿/*
+Copyright 2020 Joe-Elckooo
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+using jnsCommandLine;
+
+
+namespace jnsTxtRep
+{
+  class Program
+  {
+
+    ///****************************************************************************
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(string[] args)
+    {
+      List<string> Files = new List<string>();
+
+      string src_text = null;
+      string dst_text = null;
+      string prefix_name = null;
+      string suffix_name = null;
+      StringComparison comparation = StringComparison.InvariantCultureIgnoreCase;
+      var encoding = Encoding.UTF8;
+
+      if (!__commands(args))
+      {
+        args = null;
+
+        string app_name = Path.GetFileNameWithoutExtension(AppDomain.CurrentDomain.FriendlyName);
+        string conf = Path.Combine(Directory.GetCurrentDirectory(), app_name + ".conf");
+
+        if (File.Exists(conf))
+        {
+          List<string> lines = new List<string>();
+
+          bool r = false;
+          for (int i = 0; i < 2; i++)
+          {
+            lines.Clear();
+
+            try
+            {
+              using (StreamReader reader = new StreamReader(conf, encoding))
+                while (!reader.EndOfStream)
+                {
+                  string l = reader.ReadLine().Trim();
+                  if (string.IsNullOrEmpty(l)) continue;
+
+                  if (l.ToUpper().IndexOf("-CP:") == 0)
+                  {
+                    string v = l.Substring(4);
+                    if (r = __set_encoding(v)) break;
+                  }
+                  else lines.Add(l);
+                }
+              if (!r) i = 2;
+            }
+            catch { }
+          }
+
+          args = lines.ToArray();
+        }
+
+        if ((args == null) || !__commands(args))
+        {
+          __print_help();
+          return;
+        }
+      }
+
+      if (string.IsNullOrEmpty(prefix_name) && string.IsNullOrEmpty(suffix_name)) prefix_name = "_";
+
+      StringBuilder buff = new StringBuilder();
+
+
+      //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      #region Search and replace procedure
+
+      foreach (var src in Files)
+      {
+        try
+        {
+          string dst = prefix_name + Path.GetFileNameWithoutExtension(src) + suffix_name;
+          string ext = Path.GetExtension(src);
+          string path = Path.GetDirectoryName(src);
+
+          dst = Path.Combine(path, dst + ext);
+
+          using (var reader = new StreamReader(src, encoding, true))
+          using (var writer = new StreamWriter(dst, false, reader.CurrentEncoding))
+          {
+            while (!reader.EndOfStream)
+            {
+              string line = reader.ReadLine();
+
+              line = __replace(line, src_text, dst_text);
+              writer.WriteLine(line);
+            }
+          }
+        }
+        catch (Exception ex)
+        {
+          Console.WriteLine(ex);
+        }
+      }
+
+      #endregion
+
+
+      //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      #region Helper functions
+
+      //----->>>
+      void __print_help() => Console.WriteLine(Resource.HelpText);
+
+      //----->>>
+      bool __set_encoding(string v)
+      {
+        try
+        {
+          if (int.TryParse(v, out var cp))
+          {
+            if (encoding.CodePage != cp)
+            {
+              encoding = Encoding.GetEncoding(cp);
+              return true;
+            }
+          }
+          else
+          {
+            if (!encoding.HeaderName.Equals(v, StringComparison.InvariantCultureIgnoreCase))
+            {
+              encoding = Encoding.GetEncoding(v);
+              return true;
+            }
+          }
+        }
+        catch
+        {
+          Console.WriteLine("? Unknown code page (-cp): {0}", v);
+        }
+
+        return false;
+      }
+
+      //----->>>
+      bool __commands(string[] command_line)
+      {
+        var commands = CommandLine.ParseCommandLine(command_line);
+
+        foreach (var c in commands)
+        {
+          string key = c.Key?.ToUpper();
+          string value = c.Value?.Trim();
+
+          if (key == "S") src_text = value;
+          else if (key == "R") dst_text = value;
+          else if (key == "PN") prefix_name = value;
+          else if (key == "SN") suffix_name = value;
+          else if (key == "CS") comparation = StringComparison.InvariantCulture;
+          else if (key == "CP")
+          {
+            __set_encoding(value);
+          }
+          else if ((key == null) && (!string.IsNullOrEmpty(value)))
+          {
+            int i = value.IndexOfAny(new char[] { '*', '?' });
+            if (i >= 0) GetFiles(value, Files);
+            else if (File.Exists(value)) Files.Add(value);
+          }
+          else if (key == "H")
+          {
+            __print_help();
+            Environment.Exit(0);
+            return false;
+          }
+        }
+
+        return !(string.IsNullOrEmpty(src_text) || string.IsNullOrEmpty(dst_text) || (Files.Count == 0));
+      }
+
+      //----->>>
+      string __replace(string line, string src, string dst)
+      {
+        int i = line.IndexOf(src, comparation);
+        if (i < 0) return line;
+
+        int j = 0;
+        buff.Clear();
+
+        do
+        {
+          buff.Append(line.Substring(j, i - j));
+          buff.Append(dst);
+          j = i + src.Length;
+          if (j > line.Length) return buff.ToString();
+
+          i = line.IndexOf(src, j, comparation);
+        } while (i > 0);
+
+        buff.Append(line.Substring(j));
+
+        return buff.ToString();
+      }
+
+      #endregion
+
+    }
+
+    ///****************************************************************************
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="file_mask"></param>
+    /// <param name="list"></param>
+    static void GetFiles(string file_mask, List<string> list)
+    {
+      string path = Path.GetDirectoryName(file_mask);
+      if (string.IsNullOrEmpty(path)) path = Directory.GetCurrentDirectory();
+
+      foreach (var f in Directory.EnumerateFiles(path, file_mask, SearchOption.TopDirectoryOnly))
+      {
+        list.Add(f);
+      }
+    }
+
+
+  }
+}
